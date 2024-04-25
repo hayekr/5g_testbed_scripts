30d6fb8dcee25331b820996f5dfd7894741497e6
Robert Hayek <hayekr2@gmail.com>
2024-04-25 16:39:56 -0500

    Remove dup scripts

--- start_telit_qmicli.sh
+++
@@ -1,7 +0,0 @@
-sudo qmicli --device=/dev/cdc-wdm1 --device-open-proxy --dms-set-operating-mode=low-power
-sudo qmicli --device=/dev/cdc-wdm1 --device-open-proxy --dms-set-operating-mode=online
-sleep 4
-sudo qmicli --device=/dev/cdc-wdm1 --device-open-proxy --wds-start-network="ip-type=4,apn=oai" --client-no-release-cid
-sleep 1
-sudo udhcpc -q -f -n -i wwan0
-
--- stop_telit_qmicli.sh
+++
@@ -1,3 +0,0 @@
-sudo qmicli -d /dev/cdc-wdm1 --dms-set-operating-mode="low-power"
-sudo qmi-network /dev/cdc-wdm1 stop
-
